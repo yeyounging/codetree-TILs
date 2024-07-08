ea45fc9bc81e24986cf42ef71e8eec8506b1b5ec
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-07-08 23:37:50 +0900

    Title: 두 개의 동일한 수열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240708/두 개의 동일한 수열/two-equal-series.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+const n = input[0];
+const numbers1 = input[1].split(" ").map(Number);
+const numbers2 = input[2].split(" ").map(Number);
+let flag = false;
+
+for(let num of numbers1){
+    flag = false;
+    for(let num2 of numbers2){
+        if (num == num2) {
+            flag= true;
+            break;
+        }
+    }
+    if(flag==false){
+        console.log("No");
+        return 0;
+}
+console.log("Yes");
