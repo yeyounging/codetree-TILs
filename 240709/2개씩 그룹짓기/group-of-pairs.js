f73a8a435e83f55e1a2e61c7a8049b13794268bc
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-07-09 00:08:00 +0900

    Title: 2개씩 그룹짓기, Time: 162ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240709/2개씩 그룹짓기/group-of-pairs.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().split("\n");
+const n = Number(input[0]);
+const arr = input[1].split(" ").map(Number);
+let max = 0;
+
+const cmp = (prev,cur) => {
+    return prev-cur;
+}
+
+arr.sort(cmp);
+
+for(i = 0; i < n; i++){
+    if(max < arr[i] + arr[(n*2-1)-i])
+    max = arr[i] + arr[(n*2-1)-i]
+}
+
+console.log(max)
