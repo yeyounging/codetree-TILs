77c0bebffe30048c8c864a73a14ec9e9970c4369
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-07-20 02:06:51 +0900

    Title: 정렬된 숫자 위치 알아내기, Time: 190ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240720/정렬된 숫자 위치 알아내기/indices-of-sorted-array.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+const length = Number(input[0].trim());
+const arr = input[1].trim().split(" ").map(Number);
+const isSorted = Array(length).fill(false);
+const ans = Array(length);
+
+let min_index= 0;
+let cnt = 1;
+let flag = true;
+while(flag){
+    for(let i =0 ; i<length;i++){
+        if(isSorted[i] ===false) {
+        min_index=i; 
+        break;
+        }
+        flag = false;
+    }
+    if(!flag) break;
+    for(let j=0;j<length;j++){
+        if(isSorted[j]===false && arr[min_index]>arr[j]) min_index=j;
+    }
+    ans[min_index] = cnt++;
+    isSorted[min_index] = true;
+}
+console.log(ans.join(" "));
